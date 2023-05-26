914b1d255a29d018a75d8c794e1fd3037e9c7890
letyresina <leticiaresina@gmail.com>
2023-05-26 15:18:41 -0300

    Criado o arquivo de Python e adicionado algumas funcionalidades já

---
+++ arquivo.py
@@ -0,0 +1,13 @@
+# Importações feitas para auxiliar o processo do programa
+
+import time # intervalo de tempo para o usuário processar as informações
+
+# Funções
+
+
+
+# Mensagem de boas vindas, fora do while
+print("Seja bem-vindo(a)! Eu me chamo Alice, uma inteligência artificial produzida pela SFOME!")
+print("Para prosseguirmos, será necessário a realização do seu cadastro! Vamos lá?!")
+
+# Programa principal    
