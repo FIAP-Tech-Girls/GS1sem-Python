74dd0cbf05ef7767588651122588c8948e79c0bc
letyresina <leticiaresina@gmail.com>
2023-06-05 20:24:34 -0300

    feito os testes de previsão de vida útil

--- teste/teste6.py
+++ teste/teste6.py
@@ -105,15 +105,104 @@ def previsaoVidaUtil():
 
                 # Caso sorvetes
                 elif alimentoPerecivelCongelado == 2:
-                    print("Continua")
+                    print("O transporte de sorvete requer cuidados especiais para garantir que o produto seja mantido em condições ideais, preservando sua textura, sabor e qualidade.")
+                    time.sleep(2)
+
+                    print("O sorvete deve ser mantido em temperaturas muito baixas, geralmente abaixo de -18°C (-0,4°F), para evitar a sua descongelação.")
+                    time.sleep(2)
+
+                    print("Evite agitações excessivas e movimentos bruscos durante o transporte, pois isso pode afetar a textura e a qualidade do sorvete. Certifique-se de que os pacotes de sorvete estejam devidamente acomodados e protegidos contra impactos.")
+                    time.sleep(2)
+
+                    print("Ao chegar ao destino, o sorvete deve ser imediatamente transferido para um freezer ou armazenamento refrigerado para manter sua qualidade. Evite descongelar e congelar repetidamente o sorvete, pois isso pode afetar negativamente sua textura e sabor.")
+                    time.sleep(2)
 
                 # Caso produtos prontos
                 elif alimentoPerecivelCongelado == 3:
-                    print("Continua")
+                    print("O transporte de produtos prontos, como pães e nuggets congelados, requer considerações específicas para manter sua qualidade e segurança.")
+                    time.sleep(2)
+
+                    print("Verifique as especificações do fabricante para determinar a temperatura de transporte recomendada. Geralmente, pães e nuggets congelados devem ser mantidos a temperaturas abaixo de -18°C (-0,4°F) para evitar a deterioração e o crescimento bacteriano.")
+                    time.sleep(2)
+
+                    print("Utilize embalagens isoladas termicamente, como caixas de isopor ou contêineres apropriados para produtos congelados. Isso ajuda a manter a temperatura baixa durante o transporte e reduz a exposição ao calor externo.")
+                    time.sleep(2)
+
+                    print("Evite descongelar parcialmente os produtos durante o transporte. Isso pode comprometer a qualidade e segurança dos alimentos. Certifique-se de que o veículo de transporte seja adequado e capaz de manter a temperatura constante durante todo o percurso.")
+                    time.sleep(2)
 
         # Caso alimento não perecível (que não estraga fácil)
         elif alimento == 2:
-            print("Continua")
+                print("Alimentos não perecíveis")
+                print("Escolha uma das opções abaixo para prosseguir!")
+                print("\n 1 - Arroz \n 2 - Milho enlatado \n 3 - Leite em pó \n") # Sujeito à mudanças no futuro
+
+                alimentoNaoPerecivel = int(input("Informe a opção desejada: "))
+                if (alimentoNaoPerecivel < 1) or (alimentoNaoPerecivel > 3):
+                    raise TypeError
+                
+                # Caso arroz
+                elif alimentoNaoPerecivel == 1:
+                    print("O arroz é um alimento não perecível amplamente consumido em todo o mundo.")
+                    time.sleep(2)
+
+                    print("O arroz pode ser armazenado e transportado em temperatura ambiente, entre 15°C e 25°C. Evite expor o arroz a temperaturas extremas, como calor excessivo ou frio intenso, pois isso pode afetar sua qualidade e textura.")
+                    time.sleep(2)
+
+                    print("O arroz deve ser mantido em um local seco, pois a umidade pode levar à formação de mofo e ao apodrecimento dos grãos.")
+                    time.sleep(2)
+
+                    print("A exposição à luz solar direta pode afetar a cor e o sabor do arroz ao longo do tempo. Portanto, é recomendável armazenar o arroz em um local escuro ou em recipientes opacos que bloqueiem a entrada de luz.")
+                    time.sleep(2)
+
+                    print("Para evitar a infestação de insetos ou roedores, é importante armazenar o arroz em um local limpo e protegido. Utilize recipientes herméticos ou sacos de armazenamento fechados para impedir a entrada de pragas.")
+                    time.sleep(2)
+
+                    print("O arroz tem a capacidade de absorver odores e sabores do ambiente ao seu redor. Portanto, é recomendável armazená-lo longe de produtos com odores fortes, como produtos químicos, detergentes ou alimentos com cheiros intensos.")
+                    time.sleep(2)
+
+                # Caso milho enlatado
+                elif alimentoNaoPerecivel == 2:
+                    print("O milho enlatado é um alimento não perecível que passa por um processo de conservação adequado para garantir sua durabilidade.")
+                    time.sleep(2)
+
+                    print("O milho em lata pode ser armazenado e transportado em temperatura ambiente, entre 15°C e 25°C. Evite expor as latas de milho a temperaturas extremas, como calor excessivo ou frio intenso, pois isso pode comprometer a qualidade e a segurança do produto.")
+                    time.sleep(2)
+
+                    print("Durante o transporte, é importante manipular as latas de milho com cuidado para evitar danos físicos. Evite quedas, impactos ou compressão excessiva que possam amassar ou danificar as embalagens.")
+                    time.sleep(2)
+
+                    print("É importante manter as latas de milho em um ambiente seco para evitar a formação de mofo ou corrosão nas embalagens. Evite locais úmidos ou com alta umidade relativa do ar durante o armazenamento e transporte.")
+                    time.sleep(2)
+
+                    print("O milho enlatado deve ser protegido da exposição direta à luz solar, pois a luz pode afetar a cor e a qualidade do produto ao longo do tempo. Armazene as latas de milho em locais escuros ou utilize embalagens opacas que bloqueiem a entrada de luz.")
+                    time.sleep(2)
+
+                    print("Ao armazenar milho enlatado, é recomendável seguir o princípio da primeiro a entrar, primeiro a sair (FIFO, do inglês first in, first out). Isso significa que você deve utilizar as latas de milho mais antigas antes das mais novas, garantindo assim que o estoque seja consumido dentro do prazo de validade.")
+                    time.sleep(2)
+
+                # Caso leite em pó
+                elif alimentoNaoPerecivel == 3:
+                    print("O leite em pó é um produto desidratado que tem uma vida útil prolongada e requer condições adequadas de armazenamento e transporte para manter sua qualidade.")
+                    time.sleep(2)
+
+                    print("O leite em pó deve ser armazenado e transportado em temperatura ambiente estável, entre 15°C e 25°C. Evite exposição a variações extremas de temperatura, como calor excessivo ou frio intenso, pois isso pode afetar a qualidade do produto. Manter uma temperatura constante é importante para evitar a absorção de umidade.")
+                    time.sleep(2)
+
+                    print("O leite em pó é altamente suscetível à absorção de umidade, o que pode resultar na formação de grumos e deterioração do produto. Portanto, é essencial armazená-lo em um local seco, longe de fontes de umidade, como pias, janelas ou áreas úmidas. Utilizar recipientes hermeticamente fechados também ajuda a proteger o leite em pó da umidade.")
+                    time.sleep(2)
+
+                    print("O leite em pó é sensível à luz, especialmente à luz solar direta, pois pode levar a alterações na cor e no sabor do produto. Portanto, é recomendável armazenar o leite em pó em um local escuro ou em recipientes opacos que bloqueiem a entrada de luz.")
+                    time.sleep(2)
+
+                    print("O leite em pó tem a capacidade de absorver odores e sabores do ambiente ao seu redor. Portanto, é importante armazená-lo longe de produtos com odores fortes, como produtos químicos, detergentes ou alimentos com cheiros intensos. Armazená-lo em um recipiente hermético também ajuda a evitar a contaminação cruzada de odores e sabores.")
+                    time.sleep(2)
+
+                    print(" Para evitar a infestação de insetos ou roedores, é importante armazenar o leite em pó em um local limpo e protegido. Utilize recipientes herméticos ou sacos de armazenamento fechados para impedir a entrada de pragas.")
+                    time.sleep(2)
+
+                    print("Durante o transporte, é importante manipular o leite em pó com cuidado para evitar danos físicos à embalagem e ao produto. Evite quedas, impactos e vibrações excessivas que possam resultar em amassamento ou ruptura da embalagem.")
+                    time.sleep(2)
 
         # Caso o usuário precise de ajuda na categoria para escolher
         elif alimento == 3:
